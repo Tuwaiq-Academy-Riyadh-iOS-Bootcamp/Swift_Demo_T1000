2fcffce0d68018edae5c5036ed20d98476faf1eb
tuwiqBootcamp <abdullah.ali.ruh.s@tuwaiq.edu.sa>
2021-10-11 10:59:21 +0300

    Abdullah alnutayfi

--- swift_demo_T1000/main.swift
+++ swift_demo_T1000/main.swift
@@ -7,8 +7,15 @@
 
 import Foundation
 
-print("Enter your salary: ", terminator: "")
-let salary = Utils.readInt()
-let annualSalary = salary * 12
-print("Annual Salary:", annualSalary)
+//print("Enter your salary: ", terminator: "")
+//let salary = Utils.readInt()
+//let annualSalary = salary * 12
+//print("Annual Salary:", annualSalary)
 
+print("Enter student mark:")
+let mark = Utils.readDouble()
+print("Enter Full mark:")
+let fullMark = Utils.readInt()
+
+var pct = (mark/Double(fullMark)) * 100
+ print("PCT: \(pct)")
