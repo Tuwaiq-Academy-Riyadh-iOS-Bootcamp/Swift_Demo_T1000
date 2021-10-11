58523e813b5976b51fa975a1eb9160be23d886b9
OmarInCS <omarincs@gmail.com>
2021-10-11 09:15:45 +0300

    add missing file

---
+++ swift_demo_T1000/utils.swift
@@ -0,0 +1,28 @@
+//
+//  utils.swift
+//  demo_T1000
+//
+//  Created by user on 9/26/21.
+//
+
+import Foundation
+
+class Utils {
+
+    static func readInt() -> Int {
+        return Int(readLine()!)!
+    }
+    
+    static func readFloat() -> Float {
+        return Float(readLine()!)!
+    }
+    
+    static func readDouble() -> Double {
+        return Double(readLine()!)!
+    }
+    
+    static func readString() -> String {
+        return readLine()!
+    }
+
+}
