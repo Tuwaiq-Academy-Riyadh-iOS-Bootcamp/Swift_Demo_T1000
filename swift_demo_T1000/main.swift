0140bba53c5bf00d144ba070d17019382d5a81a2
AlDanah Aldohayan <aldanahaldohayan@AlDanahs-MacBook-Pro.local>
2021-10-11 11:24:58 +0300

    Day#2

--- swift_demo_T1000/main.swift
+++ swift_demo_T1000/main.swift
@@ -7,13 +7,5 @@
 
 import Foundation
 
-print("Day #2 Morning Class")
 
-// double: input mark , int: full mark input , (pet mark/full mark) * 100
-print("Write your mark: ", terminator: "")
-let mark = Utils.readDouble()
-print("Write your full mark: ", terminator: "")
-let fullMark = Utils.readInt()
 
-let pet = round((mark/Double(fullMark)) * 100)
-print("%\(pet)")
---
+++ swift_demo_T1000/week1/day#2/pct_marks.swift
@@ -0,0 +1,20 @@
+//
+//  per_marks.swift
+//  swift_demo_T1000
+//
+//  Created by AlDanah Aldohayan on 11/10/2021.
+//
+
+import Foundation
+//
+//print("Day #2 Morning Class")
+//
+//// double: input mark , int: full mark input , (pet mark/full mark) * 100
+//print("Write your mark: ", terminator: "")
+//let mark = Utils.readDouble()
+//print("Write your full mark: ", terminator: "")
+//let fullMark = Utils.readInt()
+//
+//let pet = round( mark/Double(fullMark) * 100) // #2 delete round()
+//print("%\(pet)")
+//// #1 or print ("%", round(pet*100) /100) to have two dec numbers .11 not .0
