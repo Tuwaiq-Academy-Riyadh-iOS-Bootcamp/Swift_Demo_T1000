0e592c287929948bb0976232c448818f26979b52
OmarKTuwaiq <Omar.Abdelkarim@tuwaiq.edu.sa>
2021-10-19 12:58:20 +0300

    add extensions missing

--- swift_demo_T1000/week2/day3/test_extensions.swift
+++ swift_demo_T1000/week2/day3/test_extensions.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+
+//extension String {
+//    
+//    subscript(_ idx: Int) -> Character {
+//        
+//        let sIdx = self.index(self.startIndex, offsetBy: idx)
+//        
+//        return self[sIdx]
+//    }
+//}
+//
+//
+//extension Double {
+//    
+//    func pow(_ n: Int) -> Double {
+//        
+//        var result = 1.0
+//        for _ in 0..<n {
+//            result *= self
+//        }
+//        
+//        return result
+//    }
+//    
+//}
+//
+//var name = "Omar"
+//print(name[0])
+//print(name[1])
+////print(name[-1])
+////print(name[-2])
+//
+//let x = 5.0
+//print(pow(x, 3))
+//print(x.pow(3))
